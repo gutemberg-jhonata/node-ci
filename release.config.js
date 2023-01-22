4fccbff805feec2bb79f47d43851a00d7a23b8d5
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2023-01-22 15:37:19 -0300

    refactor: remove semantic-release-docker plugin

--- release.config.js
+++ release.config.js
@@ -3,20 +3,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-    [
-      "@eclass/semantic-release-docker",
-      {
-        baseImageName: "node-ci",
-        registries: [
-          {
-            url: "docker.io",
-            imageName: `docker.io/${process.env.DOCKER_USERNAME}/node-ci`,
-            user: process.env.DOCKER_USERNAME,
-            password: process.env.DOCKER_PASSWORD
-          }
-        ]
-      }
-    ]
+    "@semantic-release/github"
   ]
 }
