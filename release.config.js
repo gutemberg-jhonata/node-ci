5d3d80ff59c098e4251464181a3cadaeb9fdf810
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2023-01-22 12:39:50 -0300

    ci: add release workflow

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: ["develop"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@eclass/semantic-release-docker",
+      {
+        baseImageName: "node-ci",
+        registries: [
+          {
+            url: "docker.io",
+            imageName: `docker.io/${process.env.DOCKER_USERNAME}/node-ci`,
+            user: process.env.DOCKER_USERNAME,
+            password: process.env.DOCKER_PASSWORD
+          }
+        ]
+      }
+    ]
+  ]
+}
